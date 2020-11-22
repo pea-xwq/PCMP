06d9d2c76573e4c0286db8339f505cde8e2e034c
Angelalilyer <919849055@qq.com>
2020-11-22 12:09:19 +0800

    、冗余文件

--- src/main/java/com/ssm/domain/User.java
+++ src/main/java/com/ssm/domain/User.java
@@ -3,7 +3,32 @@ package com.ssm.domain;
 import java.io.Serializable;
 
 public class User implements Serializable {
-    private Integer telephone;
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setTelephone(String telephone){
+
+        this.telephone = telephone;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String telephone;
     private String email;
     private String userName;
     private String password;
